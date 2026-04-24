e8731e677f42500580a03cf42cdda941419174fe
SadSmileXD <phycoeco@naver.com>
2026-04-24 10:23:04 +0900

    Push
    - gitignore 설정
    - SO데이터 편한 접근 툴 제작
    - 기존에 제작한 씬 이동 툴 프로젝트에 넣기

---
+++ NetWorkProject/Assets/04SO/SO_Scripts/NewScriptableObjectScript.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using System;
+[CreateAssetMenu(fileName = "NewScriptableObjectScript", menuName = "Scriptable Objects/NewScriptableObjectScript")]
+public class NewScriptableObjectScript : ScriptableObject
+{
+    public int a;
+    public int b;
+    public dbdata ddbb;
+    public Transform ttt;
+
+}
